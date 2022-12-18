c667d4c3dc77fcd32d85da052cff35a49c1fd1a6
Co van Leeuwen <coo@covle.com>
2022-12-18 12:39:17 +0700

    to the window, to the wall

--- src/lib/translate/sanitizeHtml.ts
+++ src/lib/translate/sanitizeHtml.ts
@@ -1,6 +1,6 @@
 import DOMPurify from 'isomorphic-dompurify';
 
 export function sanitizeHtml(input: string): string {
-	// return 'NOT PURIFIED';
+	if (typeof window === undefined) return 'NOT PURIFIED';
 	return DOMPurify.sanitize(input);
 }
